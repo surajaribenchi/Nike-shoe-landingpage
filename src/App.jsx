b7fab77eed6535b671839c196561e44ee9ad2ba6
surajaribenchi <surajaribenchi@gmail.com>
2023-09-28 16:40:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Navigation from './component/Navigation';
+import HeroSection from './component/Hero';
+const App = () => {
+    return (
+        <div>
+          <Navigation/> 
+          <HeroSection/>
+        </div>
+    );
+};
+export default App;
